426e9514de03c0bc103755cf4d989a5babe4004b
kaynzhel <crayzhel_misay@yahoo.com>
2021-04-04 22:28:04 -0600

    Update moneyForTime.py

--- moneyForTime.py
+++ moneyForTime.py
@@ -1,39 +1,51 @@
 '''
-Last edited: March 4, 2021
+Last edited: April 04, 2021
 '''
 import os
-from time import time, ctime
+from getInterface import Interface
 
 def main():
     program = Program()
     program.run()
-    
+
 class Program:
     def __init__(self):
         self.continue_prog = True
-        self.money = 0
-        self.prize_amt = 0
-        self.time_mins = 0
-        self.input_time = 0
-        self.total_time = 0
-        self.file_name = ''
         self.input = ''
-        self.current_time = ''
-        self.date_file_name = ''
+        self.interface = Interface() # calls the class Interface from getInterface
     
     def run(self):
+    	# a method that runs the program
         self.clear()
-        self.getFile()
+        self.signIn()
         while self.continue_prog:
             while not self.input in ('C', 'c', 'A', 'a', 'L', 'l'):
-                self.getInstructions()
+                self.instructions()
             if self.input == 'C' or self.input == 'c':
-                self.checkMoney()
+                self.interface.checkMoney()
             elif self.input == 'A' or self.input == 'a':
-                self.addTime()
+                self.interface.addTime()
             else:
-                self.getLastUpdate()
+                self.interface.getLastUpdate()
             self.decideContinue()   
+        self.interface.closeDB()
+
+    def signIn(self):
+    	# a method that runs the opening statement of the program
+        proper_inputs = ('L','l','S','s')
+        user_decide = ''
+        print("LOG IN or SIGN UP")
+        while user_decide not in proper_inputs:
+            user_decide = input("Press L/l to log in or S/s to sign up: ")
+        if user_decide == 'L' or user_decide == 'l': self.interface.getID()
+        else: self.interface.makeID()
+        
+    def instructions(self):
+        print("\nWhat do you want to do?")
+        print("Press C if you want to check your balance.")
+        print("Press A if you want to add your time.")
+        print("Press L if you want to see when you last updated.")
+        self.input = input("Key: ")
 
     def decideContinue(self):
         proper_inputs = ('Y','y','N','n')
@@ -47,116 +59,10 @@ class Program:
             self.clear()
 
     def resetVariables(self):
-        self.money = 0
-        self.prize_amt = 0
-        self.time_mins = 0
-        self.input_time = 0
-        self.total_time = 0
         self.input = ''
-        self.current_time = ''  
+        self.interface.resetVariables()
 
     def clear(self):
         os.system('clear')   
-
-    def getFile(self):
-        self.file_name = input('Please enter your account name: ')
-        self.date_file_name = self.file_name + '_date'
-
-    def checkMoney(self):
-        self.getTimeMins()
-        self.getPrizeMoney()
-        self.getTotalTime()
-        self.getMoney()
-        print("\nThis is the the total time: " + str(self.total_time) + " minutes")
-        print("This is how much money you have: $ {:.2f}".format(self.money))
-
-    def addTime(self):
-        self.getInputTime()
-        self.getTotalTime()
-        print("\nThis is the time that you inputted: " + str(self.input_time))
-        print("This is the the total time: " + str(self.total_time) + " minutes")
-
-    def getInstructions(self):
-        print("\nWhat do you want to do?")
-        print("Press C if you want to check your balance.")
-        print("Press A if you want to add your time.")
-        print("Press L if you want to see when you last updated.")
-        self.input = input("Key: ")
-
-    def getTimeMins(self):
-        incorrect = True
-        while incorrect and self.time_mins <= 0:
-            try:
-                self.time_mins = float(input('How many minutes for a specific amount? '))
-            except:
-                print("It must be a number!")
-            else:
-                if self.time_mins <= 0:
-                    print("It must be a positive number. Try again.")
-                else:
-                    incorrect = False
-    
-    def getPrizeMoney(self):
-        incorrect = True
-        while incorrect and self.prize_amt <= 0:
-            try:
-                self.prize_amt = float(input("What's the prize reward in $? "))
-            except:
-                print("It must be a number!")
-            else:
-                if self.prize_amt <= 0:
-                    print("It must be a positive number. Try again.")
-                else:
-                    incorrect = False
-    
-    def getTotalTime(self):
-        file = open(self.file_name, 'r')
-        for line in file.readlines():
-            self.total_time += int(line)
-        file.close()
-    
-    def getMoney(self):
-        self.money = (self.total_time / float(self.time_mins)) * float(self.prize_amt)
-
-    def getInputTime(self):
-        incorrect = True
-        
-        while incorrect:
-            self.input_time = 0
-            try:
-                self.input_time += int(input('What is your time in hours? ')) * 60
-                self.current_time = time()
-                self.input_time += int(input('What is your time in minutes? '))
-            except:
-                print("It must be an integer number!")
-            else:
-                if self.input_time < 0:
-                    print("It must not be a negative number!")
-                else:
-                    incorrect = False
-        
-        file = open(self.file_name, 'a') # a is for append
-        file.write('{}\n'.format(self.input_time))
-        file.close()
-        self.addCurrentTime()
-    
-    def addCurrentTime(self):
-        file = open(self.date_file_name, 'w')
-        file.write(ctime(self.current_time))
-        file.close()
-
-    def getLastUpdate(self):
-        try:
-            file = open(self.date_file_name, 'r')
-            date = file.read()
-            file.close()
-        except:
-            print("\nYou haven't made your account! Go add a time.")
-        else:
-            file = open(self.file_name, 'r')
-            for last_time in file.readlines(): pass
-            file.close()
-            print("\nThis is the last time you updated: " + date)
-            print("This is the last time you inputted (in minutes): " + str(last_time))
 
 main()
